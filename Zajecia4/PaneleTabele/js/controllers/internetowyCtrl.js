8ffaa1110cdf86fe42fff3df643b4f053fa370b6
pm <pi14142>
2016-04-23 11:53:06 +0200

    Bajzel po zajeciach 23 april

--- Zajecia4/PaneleTabele/js/controllers/internetowyCtrl.js
+++ Zajecia4/PaneleTabele/js/controllers/internetowyCtrl.js
@@ -4,33 +4,48 @@ angular.module('myApp.controllers').controller('internetowyCtrl',
     ['$rootScope','$scope', '$http',
         function ($rootScope, $scope, $http) {
             console.log('Uruchamiam kontroler netowy');
-            $scope.posty = [];
+            $scope.M = {};
+            $scope.M.posty = [];
+            $scope.fff = '';
 
         //    Tu piszemy funkcje dostępne w view:
-            $scope.loadPosts = function() {
+            $scope.sendPostX = function() {
                 var toSave = {
-                    // token: 'abcd',
-                    // obronaId: 1234
+                    token: 'abcd',
+                    obronaId: 1234,
+                    myUser: 'admin'
                 };
                 
                 var rootUrl = 'http://jsonplaceholder.typicode.com';
                 
                 return $http({
                     url: rootUrl + '/posts',
-                    method: 'GET'
-                    // params: toSave
+                    method: 'POST',
+                    data: {
+                        title: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                        body: 'Abra kadabra',
+                        userId: 1
+                    },
+                    params: toSave
                 }).success(function(data){
-                    //tu mamy dostęp do scope
-                    // $scope.tabRezerwacji = data.result;
-                    $scope.posty = data;
+                    alert('OK, udało się wysłać post');
                 });
             };
-            
+
+
             $scope.prostyLoad = function () {
                 $http.get("http://jsonplaceholder.typicode.com/posts")
                     .success(function (data) {
-                        $scope.posty = data;
+                        $scope.M.posty = data;
                     })
+            };
+
+            $scope.isValidPesel = function (ppesel){
+                if (ppesel.length==11) {
+                    if (!isNaN(parseFloat(ppesel)) && isFinite(ppesel))
+                        return true;
+                }
+                return false;
             }
 
             
